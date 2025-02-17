b77fa9649f4a6d919a7172030946c16e25b05f44
Rajesh-Saini-dav <rajeshsaini7042@gmail.com>
2025-02-17 20:19:41 +0530

    update portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,6 +3,13 @@ import React from 'react';
 import { FiCoffee, FiCodesandbox, FiActivity } from "react-icons/fi";
 
 
+const portfolio =[
+    { name: 'High experience', icon: <FiActivity className='text-orange-500' /> },
+      { name: 'High experience', icon: <FiCodesandbox className='text-blue-500' /> },
+        { name: 'High experience', icon: <FiCoffee className='text-purple-500' /> },
+]
+
+
 function Portfolio() {
   return (
     <section id='portfolio' className='py-24'>
@@ -14,36 +21,21 @@ function Portfolio() {
 
         
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-        <div className='bg-gray-50 px-8 py-10 rounded-md shadow-lg hover:shadow-gray-500 transition ease-out duration-500'>
-                    <div className='w-20 py-6 flex justify-center text-2xl bg-gray-100 rounded-md mb-4 hover:bg-gray-300 transition ease-out duration-500'>
-                      <FiActivity />
-                    </div>
-                    <h4 className='font-medium text-gray-700 text-lg mb-4'>High experience</h4>
-                    <p className='font-normal text-gray-500 text-md'>
-                      Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed do eiusmod tempor <br /> incididunt ut labore et dolore magna aliqua.
-                    </p>
-                  </div>
-                  <div className='bg-gray-50 px-8 py-10 rounded-md'>
-                    <div className='w-20 py-6 flex justify-center text-2xl bg-gray-100 rounded-md mb-4'>
-                      <FiCodesandbox />
-                    </div>
-                    <h4 className='font-medium text-gray-700 text-lg mb-4'>Useful sandboxes</h4>
-                    <p className='font-normal text-gray-500 text-md'>
-                      Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed do eiusmod tempor <br /> incididunt ut labore et dolore magna aliqua.
-                    </p>
-                  </div>
-                  <div className='bg-gray-50 px-8 py-10 rounded-md'>
-                    <div className='w-20 py-6 flex justify-center text-2xl bg-gray-100 rounded-md mb-4'>
-                    <FiCoffee />
-                    </div>
-                    <h4 className='font-medium text-gray-700 text-lg mb-4'>Success side projects</h4>
-                    <p className='font-normal text-gray-500 text-md'>
-                      Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed do eiusmod tempor <br /> incididunt ut labore et dolore magna aliqua.
-                    </p>
-                  </div>
 
+          {portfolio.map((port) => (
+            <div className='bg-gray-50 px-8 py-10 rounded-md shadow-lg hover:shadow-gray-500 transition ease-out duration-500'>
+            <div className='w-20 py-6 flex justify-center text-2xl bg-gray-100 rounded-md mb-4 hover:bg-gray-400 transition ease-out duration-500'>
+              {port.icon}
+            </div>
+            <h4 className='font-medium text-gray-700 text-lg mb-4'>{port.name}</h4>
+            <p className='font-normal text-gray-500 text-md'>
+              Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed do eiusmod tempor <br /> incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
 
+          ))}
 
+ 
         </div>
       </div>
     </section>
